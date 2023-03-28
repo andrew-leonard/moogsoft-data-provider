db375165ef53484a90e7fb6c82a9e9e753d069f6
doug.blinkhorn <doug.blinkhorn@moogsoft.com>
2023-03-28 11:31:21 -0700

    fixed similarity not triggering between the two incidents with unique alerts

--- similarIncidents.js
+++ similarIncidents.js
@@ -1,106 +1,94 @@
-const axios = require('axios');
-const randomWords = require('random-words');
+const axios = require("axios");
+const randomWords = require("random-words");
 
 exports.similarIncidents = function(apiKey) {
 
     const sendFn = async function(event) {
         try {
             await axios({
-                method: 'post',
-                url: 'https://api.dev.moogsoft.cloud/v1/integrations/events',
+                method: "post",
+                url: "https://api.dev.moogsoft.cloud/v1/integrations/events",
                 data: event,
                 headers: {
-                    'Accept': 'application/json',
+                    "Accept": "application/json",
                     apiKey: apiKey,
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json"
                 }
             });
         } catch (e) {
             if (e.response.status === 403) {
-                console.error('API Key invalid. Get new one from your instance.');
+                console.error("API Key invalid. Get new one from your instance.");
                 process.exit();
             }
-            console.log('Caught an error: ', e);
+            console.log("Caught an error: ", e);
         }
     }
 
     const severities = [
-        'minor',
-        'major',
-        'warning',
-        'critical'
+        "minor",
+        "major",
+        "warning",
+        "critical"
     ];
 
     const sources = [
-        'dilithiumSensor',
-        'filterMonitor',
-        'shieldAware',
-        'commsCheck'];
+        "dilithiumSensor",
+        "filterMonitor",
+        "shieldAware",
+        "commsCheck"
+    ];
 
     const comparisonKeyValues = [
         {
-            check: 'propulsion',
-            class: 'engineering',
-            manager: 'warpCore',
-            type: 'dilithium'
+            "check": "propulsion",
+            "class": "engineering",
+            "manager": "warpCore",
+            "type": "dilithium"
         },
         {
-            check: 'oxygen',
-            class: 'lifeSupport',
-            manager: 'filter',
-            type: 'occluded'
+            "check": "oxygen",
+            "class": "lifeSupport",
+            "manager": "filter",
+            "type": "occluded"
         },
         {
-            check: 'defense',
-            class: 'shields',
-            manager: 'deflectors',
-            type: 'signal'
+            "check": "defense",
+            "class": "shields",
+            "manager": "deflectors",
+            "type": "signal"
         },
         {
-            check: 'communication',
-            class: 'subspace',
-            manager: 'array',
-            type: 'capacity'
+            "check": "communication",
+            "class": "subspace",
+            "manager": "array",
+            "type": "capacity"
         },
-        {
-            check: 'simulation',
-            class: 'holodeck',
-            manager: 'projector',
-            type: 'power'
-        },
-        {
-            check: 'lifeSupport',
-            class: 'replicator',
-            manager: 'materializer',
-            type: 'overheating'
-        }
+        // {
+        //     "check": "simulation",
+        //     "class": "holodeck",
+        //     "manager": "projector",
+        //     "type": "power"
+        // },
+        // {
+        //     "check": "lifeSupport",
+        //     "class": "replicator",
+        //     "manager": "materializer",
+        //     "type": "overheating"
+        // }
     ];
-
-    const eventData = {
-        description: 'CPU spike to 75%',
-        time: Math.floor(Date.now() / 1000),
-        alias: 'alias',
-        location: {
-          geo_coordinates: { lat: 42.3665325, long: -71.0501863 },
-          region: 'us-west-1'
-        },
-        manager_id: '1234',
-        tags: {
-            'jira-ticket': 'RGB-2654',
-            'error-code': 'x0F391',
-            'default-sprint': 'dazzling-wright',
-            'scrum-team': 'spifftacular-brainiacs'
-        }
-    };
 
     function generateEvents(eventCount, sourceIndex, compKeyIndex) {
         const events = [];
-        for (count = 0; count <= eventCount; count++) {
+        for (count = 0; count < eventCount; count++) {
             events.push({
-                source: sources[sourceIndex],
-                severity: severities[Math.floor(Math.random() * 4)],
-                service: randomWords({ exactly: 1, wordsPerString: 1 }).toString(),
-                ...eventData,
+                "source": sources[sourceIndex],
+                "severity": severities[Math.floor(Math.random() * 4)],
+                "service": randomWords({ exactly: 1, wordsPerString: 1 }).toString(),
+                "description": randomWords({
+                    exactly: 1,
+                    wordsPerString: 1
+                }).toString(),
+                "time": Math.floor(Date.now() / 1000),
                 ...comparisonKeyValues[compKeyIndex],
             });
         };
@@ -108,13 +96,14 @@ exports.similarIncidents = function(apiKey) {
     };
     
     const events = [
-        ...generateEvents(10, 0, 0),
-        ...generateEvents(7, 1, 1),
-        ...generateEvents(10, 2, 2),
-        ...generateEvents(3, 2, 3),
-        ...generateEvents(7, 3, 4),
-        ...generateEvents(4, 3, 5)
+        ...generateEvents(10, 0, 0),  // incident A
+        ...generateEvents(8, 1, 0),   // incident A
+        ...generateEvents(10, 2, 1),  // incident B
+        ...generateEvents(2, 2, 2),   // incident B
+        ...generateEvents(10, 3, 1),  // incident C
+        ...generateEvents(2, 3, 3)    // incident C
     ]
 
+    console.log("Creating similar incidents...")
     events.forEach(event => sendFn(event));
 };
